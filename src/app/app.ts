c79bbe176c334a599eb7b50e7b720f3ba1b7c1a2
miguelalchaar <alchaarcs@gmail.com>
2025-10-27 17:36:58 -0300

    feat(routes): configura rotas principais

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './_components/navbar/navbar';
+
+import { BaseUi } from './_components/base-ui/base-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, BaseUi],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('gwerador-certificado');
+  protected readonly title = signal('gerador-certificado');
+  exibeNavBar: boolean = true;
 }
