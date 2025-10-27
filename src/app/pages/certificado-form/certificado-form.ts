c6de0346589b6591d91f5b9a1a389d25d680e67d
miguelalchaar <alchaarcs@gmail.com>
2025-10-27 17:35:48 -0300

    feat(pages): adiciona páginas principais

---
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { PrimaryButton } from '../../_components/primary-button/primary-button';
+import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { FormsModule, NgModel } from '@angular/forms';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-certificado-form',
+  imports: [PrimaryButton, SecondaryButton, FormsModule, NgStyle],
+  templateUrl: './certificado-form.html',
+  styleUrl: './certificado-form.css',
+})
+export class CertificadoForm {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = [];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
+}
