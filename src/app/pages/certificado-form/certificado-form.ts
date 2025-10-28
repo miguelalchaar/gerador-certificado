026d0ee2120b6e28e4209945dd0305e3258bdf1c
miguelalchaar <alchaarcs@gmail.com>
2025-10-27 23:23:35 -0300

    feat: cria interface para armazenar e validar dados do certificado

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -3,6 +3,7 @@ import { PrimaryButton } from '../../_components/primary-button/primary-button';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { FormsModule, NgModel } from '@angular/forms';
 import { NgStyle } from '@angular/common';
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificado-form',
@@ -11,24 +12,33 @@ import { NgStyle } from '@angular/common';
   styleUrl: './certificado-form.css',
 })
 export class CertificadoForm {
-  nome: string = '';
+  certificado: Certificado = {
+    atividades: [],
+    nome: '',
+  };
   atividade: string = '';
-  atividades: string[] = [];
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched;
   }
 
   formValido() {
-    return this.atividades.length > 0 && this.nome.length > 0;
+    return this.certificado.atividades.length > 0 && this.certificado.nome.length > 0;
   }
 
   adicionarAtividade() {
-    this.atividades.push(this.atividade);
+    this.certificado.atividades.push(this.atividade);
     this.atividade = '';
   }
 
   excluirAtividade(index: number) {
-    this.atividades.splice(index, 1);
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit() {
+    if (!this.formValido()) {
+      return;
+    }
+    console.log(this.certificado);
   }
 }
